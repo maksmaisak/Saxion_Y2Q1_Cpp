ca575088f565b89ec5c576fe60d4f22144992de4
Maks <max.maisak@gmail.com>
2018-11-03 18:10:29 +0100

    Better font for score

--- src/systems/ScoreSystem.cpp
+++ src/systems/ScoreSystem.cpp
@@ -14,14 +14,13 @@ void setOriginNormalized(sf::Text& text, sf::Vector2f coords = {0.5f, 0.5f}) {
 void ScoreSystem::start() {
 
     m_fontPtr = std::make_unique<sf::Font>();
-    bool didLoadFont = m_fontPtr->loadFromFile("assets/Menlo.ttc");
+    bool didLoadFont = m_fontPtr->loadFromFile("assets/hyperspace/Hyperspace.otf");
     assert(didLoadFont);
 
     m_scoreTextPtr = std::make_unique<sf::Text>();
     m_scoreTextPtr->setFont(*m_fontPtr);
 
-    m_scoreTextPtr->setString("Test");
-    setOriginNormalized(*m_scoreTextPtr, {1, 0});
+    m_scoreTextPtr->setCharacterSize(72);
 
     const sf::View& view = m_engine->getWindow().getView();
     const sf::Vector2f size = view.getSize();
@@ -31,7 +30,7 @@ void ScoreSystem::start() {
 
 void ScoreSystem::draw() {
 
-    m_scoreTextPtr->setString("Score: " + std::to_string(m_score));
+    m_scoreTextPtr->setString("Score " + std::to_string(m_score));
     setOriginNormalized(*m_scoreTextPtr, {1, 0});
     m_engine->getWindow().draw(*m_scoreTextPtr);
 }
