2cd66cf3c70fce452c598cc45d1eb4279853277c
Maks <max.maisak@gmail.com>
2018-11-04 01:10:03 +0100

    Share the same bullet shape between all bullets

--- src/factory/BulletFactory.cpp
+++ src/factory/BulletFactory.cpp
@@ -11,6 +11,13 @@
 #include "DestroyTimer.h"
 #include "DrawInfo.h"
 
+static auto bulletShapePtr = []() {
+    auto ptr = std::make_shared<sf::CircleShape>(10.f, 20);
+    sf::Rect bounds = ptr->getLocalBounds();
+    ptr->setOrigin(bounds.width * 0.5f, bounds.height * 0.5f);
+    return ptr;
+}();
+
 namespace game {
 
     en::Entity makeBullet(en::Engine& engine, const sf::Vector2f& position, const sf::Vector2f& velocity) {
@@ -19,14 +26,13 @@ namespace game {
         en::Entity e = registry.makeEntity();
 
         registry.add<en::Transformable>(e).setPosition(position);
+        {
+            auto& rb = registry.add<en::Rigidbody>(e);
+            rb.velocity = velocity;
+            rb.invMass = 1.f / 0.01f;
+        }
 
-        auto& rb = registry.add<en::Rigidbody>(e);
-        rb.velocity = velocity;
-        rb.invMass = 1.f / 0.01f;
-
-        auto shapePtr = std::make_shared<sf::CircleShape>(10.f, 20);
-        shapePtr->setOrigin(0.5f, 0.5f);
-        registry.add<en::DrawInfo>(e, std::move(shapePtr));
+        registry.add<en::DrawInfo>(e, bulletShapePtr);
         registry.add<Bullet>(e);
         registry.add<DestroyTimer>(e, GameTime::now() + sf::seconds(2.f));
 
